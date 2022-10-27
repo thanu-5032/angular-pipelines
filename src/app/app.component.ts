9b54139fdadccfe3f04554251dc0748819796973
thanu-5032 <100665912+thanu-5032@users.noreply.github.com>
2022-10-27 20:18:46 +0530

    angular assignment

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  sortoption: string = '';
+  productsList: any = [
+    { productName: 'Samsung J7', price: 18000 },
+    { productName: 'Apple iPhone 6S', price: 60000 },
+    { productName: 'Lenovo K5 Note', price: 10000 },
+    { productName: 'Nokia 6', price: 15000 },
+    { productName: 'Vivo V5 Plus', price: 26000 },
+  ];
+}
